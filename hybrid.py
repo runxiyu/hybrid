1ff90ae2dac9875b6a7bdaa1ef2411d69be4ff88
Runxi Yu <me@runxiyu.org>
2024-05-14 20:49:44 +0800

    Add AGPL-3.0-only

--- hybrid.py
+++ hybrid.py
@@ -1,13 +1,33 @@
-from flask import Flask, render_template, request, redirect, abort, Blueprint
+#!/usr/bin/env python3
+#
+# Although the above shebang line exists, you should probably run it in a
+# production environment with something like gunicorn or uwsgi.
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+# https://git.runxiyu.org/runxiyu/current/hybrid.git
+#
+
+
+from flask import Flask, render_template, request, redirect, abort
 from flask.wrappers import Response
 import time, os
-bp = Blueprint('hybrid', __name__, template_folder='templates')
 app = Flask(__name__)
-app.register_blueprint(bp, url_prefix="/hybrid")
 
-@app.route('/', methods=['GET'])
+VERSION = """hybrid v0.1
+
+License: GNU Affero General Public License v3.0 only
+URL: https://git.runxiyu.org/runxiyu/current/hybrid.git"""
+
+# REMEMBER: You can only listen inside /hybrid/. Everything outside is
+#           supposed to be static.
+
+@app.route('/hybrid/', methods=['GET'])
 def index():
     return "No endpoint specified!"
+
+@app.route('/hybrid/version', methods=['GET'])
+def index():
+    return VERSION
 
 if __name__ == "__main__":
     app.run(port=8080)
